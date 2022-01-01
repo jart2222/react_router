4c5c47e12b01db4ff6f5ace2c2425909f55d5bf7
jart2222 <jart2607@gmail.com>
2022-01-01 15:34:22 -0600

    implemente la lectura de parametros de url

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { render } from "react-dom";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import App from "./App";
 import Expenses from "./routes/expenses";
+import Invoice from "./routes/invoice";
 import Invoices from "./routes/invoices";
 
 const rootElement = document.getElementById("root");
@@ -10,7 +11,18 @@ render(
     <Routes>
       <Route path="/" element={<App></App>}>
         <Route path="expenses" element={<Expenses></Expenses>}></Route>
-        <Route path="invoices" element={<Invoices></Invoices>}></Route>
+        <Route path="invoices" element={<Invoices></Invoices>}>
+        <Route path=":invoiceId" element={<Invoice />} />
+        </Route>
+      </Route>
+      <Route
+       path="*"
+       element={
+         <main style={{ padding: "1rem" }}>
+           <p>There's nothing here!</p>
+         </main>
+       } 
+      >
       </Route>
     </Routes>
   </BrowserRouter>,
--- src/routes/invoices.js
+++ src/routes/invoices.js
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import { getInvoices } from "../data";
 
 
@@ -22,6 +22,7 @@ export default function Invoices() {
             </Link>
           ))}
         </nav>
+        <Outlet></Outlet>
       </div>
     );
   }
